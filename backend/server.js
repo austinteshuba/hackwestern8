c321980cf2d399e5fed5bbbb53893d5288ce7957
austinteshuba <austinteshuba19@gmail.com>
2021-11-20 17:52:10 -0500

    Website is Hosted

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use('/', express.static(path.join(__dirname, '../frontend')));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
